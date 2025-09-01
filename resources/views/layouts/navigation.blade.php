a1a96c698fad546c2ab3cd8b3e22d441edf9ecd7
Jaylord Ferrer <simplyniceweb@gmail.com>
2025-09-01 16:58:39 +0800

    home

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
+                @if(!$user)
+                <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="ms-4">
+                    {{ __('Home') }}
+                </x-nav-link>
+                @endif
 
                 @if($user)
                 <!-- Navigation Links -->
